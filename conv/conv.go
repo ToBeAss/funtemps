bf15933c567d114cc271263449ccbcf88510c0b7
Tobias Molland <112409957+ToBeAss@users.noreply.github.com>
2023-04-14 01:08:04 +0200

    Fjernet formatering av tall

--- conv/conv.go
+++ conv/conv.go
@@ -13,39 +13,41 @@ import (
 */
 
 // Lignende funksjon som den over, men her blir unødvendige nuller (0) tatt bort
-func FloatRound(value float64) string {
-	str := fmt.Sprintf("%.2f", value)
-	return strings.TrimRight(strings.TrimRight(str, "0"), ".")
-}
+/*
+	func FloatRound(value float64) string {
+		str := fmt.Sprintf("%.2f", value)
+		return strings.TrimRight(strings.TrimRight(str, "0"), ".")
+	}
+*/
 
 // Konverteringsfunksjoner:
 
-func FarhenheitToCelsius(fahrenheit float64) string {
+func FarhenheitToCelsius(fahrenheit float64) float64 {
 	celsius := (fahrenheit - 32) * (5.0 / 9.0)
-	return FloatRound(celsius)
+	return celsius
 }
 
-func FarhenheitToKelvin(fahrenheit float64) string {
+func FarhenheitToKelvin(fahrenheit float64) float64 {
 	kelvin := (fahrenheit-32)*(5.0/9) + 273.15
-	return FloatRound(kelvin)
+	return kelvin
 }
 
-func CelsiusToFahrenheit(celsius float64) string {
+func CelsiusToFahrenheit(celsius float64) float64 {
 	fahrenheit := celsius*(9/5.0) + 32
-	return FloatRound(fahrenheit)
+	return fahrenheit
 }
 
-func CelsiusToKelvin(celsius float64) string {
+func CelsiusToKelvin(celsius float64) float64 {
 	kelvin := celsius + 273.15
-	return FloatRound(kelvin)
+	return kelvin
 }
 
-func KelvinToFahrenheit(kelvin float64) string {
+func KelvinToFahrenheit(kelvin float64) float64 {
 	fahrenheit := (kelvin-273.15)*(9/5.0) + 32
-	return FloatRound(fahrenheit)
+	return fahrenheit
 }
 
-func KelvinToCelsius(kelvin float64) string {
+func KelvinToCelsius(kelvin float64) float64 {
 	celsius := kelvin - 273.15
-	return FloatRound(celsius)
+	return celsius
 }
